6f45327acfaf1e700e01f137df0c83d0ad68a733
Jeet Sharma <jeetsharma31179@gmail.com>
2024-12-13 16:17:24 +0530

    Blazor added

---
+++ Blazor/WorkingWithBlazor/Components/Pages/Customer.razor.cs
@@ -0,0 +1,12 @@
+﻿//using Microsoft.AspNetCore.Components;
+
+//namespace WorkingWithBlazor.Components.Pages
+//{
+//    public partial class Customer : ComponentBase
+//    {
+//        public string CustomerName { get; set; } = "Jeet Sharma";
+
+//        public String ChangeTheValue => this.CustomerName = "Jeet";
+
+//    }
+//}
