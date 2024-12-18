6a4dd73c82b01de551642908ac5b7041e9526ee6
Jeet Sharma <jeetsharma31179@gmail.com>
2024-12-18 17:54:55 +0530

    done

---
+++ Blazor-Projectt/BlazorApp1/Model/BlazorRoom.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+
+        public string RoomName { get; set; }
+
+        public double Price { get; set; }
+
+        public bool isActive { get; set; }
+    }
+}
